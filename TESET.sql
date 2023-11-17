12553c96f8b406256e7891275ff0b31dc82ddc16
Mjkey2 <Mjmarentes2005@gmail.com>
2023-11-16 19:59:50 -0800

    hehe

---
+++ TESET.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Employees (
+	Emp_No INTEGER
+	Emp_Title_ID VARCHAR(5),
+	Salary INTEGER,
+	Birthday TIMESTAMP,
+	First_Name VARCHAR(15),
+	Last_Name VARCHAR (15),
+	Sex VARCHAR (6),
+	Hire_date,
+	Dept_No,
+)
