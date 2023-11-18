f1126b09e3a7ce5f9375f9ebb0c6c00a80cdf591
Mjkey2 <Mjmarentes2005@gmail.com>
2023-11-18 09:44:24 -0800

    THIS IS WEIRD4

--- TESET.sql
+++ TESET.sql
@@ -1,12 +1,12 @@
 -- is this class or table?
 CREATE TABLE Employees(
-	Emp_No INTEGER
-	Emp_Title_ID VARCHAR(5),
-	Salary INTEGER,
-	Birthday TIMESTAMP,
-	First_Name VARCHAR(15),
-	Last_Name VARCHAR (15),
-	Sex VARCHAR (6),
-	Hire_date,
-	Dept_No,
-)
+	Emp_No INTEGER NOT NULL PRIMARY KEY,
+	Emp_Title_ID VARCHAR(5) NOT NULL,
+	Salary INTEGER NOT NULL,
+	Birthday TIMESTAMP NOT NULL,
+	First_Name VARCHAR(15) NOT NULL,
+	Last_Name VARCHAR (15) NOT NULL,
+	Sex VARCHAR (6) NOT NULL,
+	Hire_date TIMESTAMP NOT NULL,
+	Dept_No VARCHAR
+);
